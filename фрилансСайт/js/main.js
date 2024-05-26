81f543199d147c0c1661f34c51c9566a5d106303
Yandiev3 <161507828+Yandiev3@users.noreply.github.com>
2024-05-26 21:29:13 +0300

    Add files via upload

---
+++ фрилансСайт/js/main.js
@@ -0,0 +1,77 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
+
+// 2. Проверка темной темы в localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+// Если меняются системные настройки, меняем тему
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme === "dark") {
+			btnDarkMode.classList.add("dark-mode-btn--active");
+			document.body.classList.add("dark");
+			localStorage.setItem("darkMode", "dark");
+		} else {
+			btnDarkMode.classList.remove("dark-mode-btn--active");
+			document.body.classList.remove("dark");
+			localStorage.setItem("darkMode", "light");
+		}
+    });
+
+// Включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
+
+
+// Кнопка обратной связи
+// document.getElementById('showForm').addEventListener('click', function() {
+//     var form = document.getElementById('feedbackForm');
+//     if (form.style.display === 'none') {
+//         form.style.display = 'block';
+//     } else {
+//         form.style.display = 'none';
+//     }
+// });
+
+// document.getElementById('sendForm').addEventListener('click', function() {
+    
+// });
+
+document.getElementById('showForm').addEventListener('click', function() {
+    var form = document.getElementById('feedbackForm');
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+});
+
+document.getElementById('sendForm').addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем отправку формы
+    var form = document.getElementById('feedbackForm');
+    form.style.display = 'none';
+    alert('Сообщение отправлено!');
+    this.reset();
+});
